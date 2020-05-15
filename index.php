4d6e38bfbcc88d6ba3ef14b4bd3e70521564869d
MrMadness2143 <Swerdor@gmail.com>
2020-05-15 15:06:34 +0100

    Update index.php
    got, multiple variables to work in a function

--- index.php
+++ index.php
@@ -9,11 +9,18 @@ require __DIR__ . '/vendor/autoload.php';
 $app = AppFactory::create();
 $app->setBasePath("/apiProjectAs"); //sets project folder as base
 
-$app->get('/elogs/{name},{name2}', function (Request $request, Response $response, $args) {
+$app->get('/elogs/date/{name}', function (Request $request, Response $response, $args) {
     extract($args);
-    // $name = $args['name'];
-    // $name2 = $args['name2'];
-    $response->getBody()->write("Hello, $name, $name2");
+    //$args['name'];
+    $response->getBody()->write("Hello,$name  no ");
+    return $response;
+});
+
+$app->get('/elogs/{errID}.{name2}', function (Request $request, Response $response, $args) {
+    extract($args);
+    $errID = $args['errID'];
+    $name2 = $args['name2'];
+    $response->getBody()->write("Hello, $errID say hello to $name2");
     return $response;
 });
 
